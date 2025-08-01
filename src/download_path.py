6b166751b1e56073febcafa2da82f44159e1978f
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2025-08-01 14:37:58 +0200

    [refactor] seperate remote acces from cmap

--- src/download_path.py
+++ src/download_path.py
@@ -1,8 +1,12 @@
-ROOT = (
-    "simplecache::zip::https://git.geo.tuwien.ac.at/api/v4/projects/1266"
-    + "/repository/files/"
-)
+ROOT = "https://git.geo.tuwien.ac.at/api/v4/projects/1266/repository/files/"
 
 
-def make_url(file, lfs="true"):
-    return ROOT + file + f"/raw?ref=main&lfs={lfs}"
+def make_url(file, lfs=True, zip=False, cache=False, verbose=True):
+    url = f"{ROOT}{file}/raw?ref=main&lfs={str(lfs).lower()}"
+    if verbose:
+        print(url)
+    if zip:
+        url = f"zip::{url}"
+    if cache:
+        url = f"simplecache::{url}"
+    return url
