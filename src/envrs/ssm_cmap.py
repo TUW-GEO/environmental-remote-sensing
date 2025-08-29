d94ef4936633067e7d9288dd963028313478ba86
MartinSchobben <schobbenmartin@gmail.com>
2025-08-29 08:32:46 +0200

    [fix] convert to package and fix imports

--- src/envrs/ssm_cmap.py
+++ src/envrs/ssm_cmap.py
@@ -1,18 +1,55 @@
-import matplotlib
+"""Surface Soil Moisture Color Map.
+
+Returns
+-------
+        Surface Soil Moisture Color Map : matplotlib.colors.LinearSegmentedColormap
+
+"""
+
+import matplotlib as mpl
 import pandas as pd
-from download_path import make_url
+
+from envrs.download_path import make_url
 
 
-def load_cmap():
-    def to_hex_str(x):
+def load_cmap() -> mpl.colors.LinearSegmentedColormap:
+    """Surface Soil Moisture Color Map.
+
+    Loading Surface Soil Moisture Color Map based on the TU Wien standard.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+        Surface Soil Moisture Color Map : matplotlib.colors.LinearSegmentedColormap
+
+    """
+
+    def to_hex_str(x: list) -> str:
+        """RGB Hex String.
+
+        Convert RGB values to hex string
+
+        Parameters
+        ----------
+        x : list
+            RGB values
+
+        Returns
+        -------
+        Hex string : str
+
+        """
         return f"#{int(x.R):02x}{int(x.G):02x}{int(x.B):02x}"
 
     path = r"colour-tables%2Fssm-continuous.ct"
-    df = pd.read_fwf(
+    color_df = pd.read_fwf(
         make_url(path, lfs="false", verbose=False), names=["R", "G", "B"], nrows=200
     )
-    brn_yl_bu_colors = df.apply(to_hex_str, axis=1).to_list()
-    return matplotlib.colors.LinearSegmentedColormap.from_list("", brn_yl_bu_colors)
+    brn_yl_bu_colors = color_df.apply(to_hex_str, axis=1).to_list()
+    return mpl.colors.LinearSegmentedColormap.from_list("", brn_yl_bu_colors)
 
 
 SSM_CMAP = load_cmap()
